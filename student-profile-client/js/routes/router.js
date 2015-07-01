8fa34cddd37ca15afcabfedb25321ad65c83a842
mathildathompson <mathildathompson288@gmail.com>
2015-07-01 20:40:18 +0100

    Added the body click event onto the body

--- student-profile-client/js/routes/router.js
+++ student-profile-client/js/routes/router.js
@@ -6,6 +6,10 @@ directory.Router = Backbone.Router.extend({
   initialize: function(){
     directory.shellView = new directory.ShellView();
     $('body').html(directory.shellView.render().el);
+    $('body').click(function(){
+      console.log('body clicked')
+      $('.dropdown').removeClass('open');
+    })
     this.content = $('#content');
   },
   home: function(){
